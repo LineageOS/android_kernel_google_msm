8c4fa43875ab49b9369071e69de4b4e1b3c1a6d2
Olav Haugan <ohaugan@codeaurora.org>
2013-04-17 16:11:31 -0700

    gpu: ion: Use correct type for variables
    Use correct type for variables to prevent overflow of signed
    type when assiging from an unsigned type.
    
    Change-Id: I0055d35c7310d111de590db3798950fd98f1b298
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- drivers/gpu/ion/ion_iommu_heap.c
+++ drivers/gpu/ion/ion_iommu_heap.c
@@ -111,7 +111,7 @@ static int ion_iommu_heap_allocate(struct ion_heap *heap,
 		struct sg_table *table;
 		int j;
 		unsigned int num_large_pages = 0;
-		long size_remaining = PAGE_ALIGN(size);
+		unsigned long size_remaining = PAGE_ALIGN(size);
 		unsigned int max_order = orders[0];
 
 		data = kmalloc(sizeof(*data), GFP_KERNEL);
