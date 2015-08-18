d8c41bc4aea508d542593f150be190d6a3c53942
Gabriele M <moto.falcon.git@gmail.com>
2015-08-18 23:49:13 +0200

    staging: prima: Fix non-debug build and switch to it
    Enable the forward declaration of debug functions in non-debug
    builds and disable a piece of code used only to print some info.
    -Wno-unused-variable is now required because of all the variables
    used in debug functions that aren't left out in non-debug builds.
    
    Change-Id: I7069f8859e4965ee82ce65c29c35349c887fd673

--- drivers/staging/prima/CORE/SME/src/sme_common/sme_Api.c
+++ drivers/staging/prima/CORE/SME/src/sme_common/sme_Api.c
@@ -4625,9 +4625,9 @@ VOS_STATUS sme_DbgWriteMemory(tHalHandle hHal, v_U32_t memAddr, v_U8_t *pBuf, v_
 }
 
 
+#ifdef WLAN_DEBUG
 void smsLog(tpAniSirGlobal pMac, tANI_U32 loglevel, const char *pString,...)
 {
-#ifdef WLAN_DEBUG
     // Verify against current log level
     if ( loglevel > pMac->utils.gLogDbgLevel[LOG_INDEX_FOR_MODULE( SIR_SMS_MODULE_ID )] )
         return;
@@ -4641,8 +4641,8 @@ void smsLog(tpAniSirGlobal pMac, tANI_U32 loglevel, const char *pString,...)
 
         va_end( marker );              /* Reset variable arguments.      */
     }
-#endif
 }
+#endif
 
 /* ---------------------------------------------------------------------------
     \fn sme_GetWcnssWlanCompiledVersion
