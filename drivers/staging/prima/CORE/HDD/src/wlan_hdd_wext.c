805ef6122865f7c1f29bdd42594989453b97fb1e
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-12-12 18:45:32 -0800

    prima: Fix array out-of-bounds & integer underflow in _iw_set_genie
    'wrqu->data.length' holds the total number of IE data buffer.
    Add a check to make sure the number of remaining data to be read is
    greater than or equal to IE length.
    
    Also, advance the buffer pointer to point to the next element only
    if next element is present.
    
    Change-Id: Ic60f3e0650f365955dab4099eb8740e9789e00cc
    CRs-Fixed: 1100132

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -1624,6 +1624,13 @@ static int iw_set_genie(struct net_device *dev,
         hddLog(VOS_TRACE_LEVEL_INFO, "%s: IE[0x%X], LEN[%d]\n",
             __func__, elementId, eLen);
 
+        if (remLen < eLen) {
+            hddLog(LOGE, "Remaining len: %u less than ie len: %u",
+                   remLen, eLen);
+            ret = -EINVAL;
+            goto exit;
+        }
+
         switch ( elementId )
          {
             case IE_EID_VENDOR:
@@ -1707,8 +1714,11 @@ static int iw_set_genie(struct net_device *dev,
 		kfree(base_genie);
                 return 0;
         }
-        genie += eLen;
         remLen -= eLen;
+
+        /* Move genie only if next element is present */
+        if (remLen >= 2)
+            genie += eLen;
     }
     EXIT();
     kfree(base_genie);
